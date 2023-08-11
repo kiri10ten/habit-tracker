26ff42ae873cee39a3e000c131ce1e83d16dc2d6
kiri10ten <126086612+kiri10ten@users.noreply.github.com>
2023-08-11 09:13:30 +0530

    inital commit

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <NuxtWelcome />
+
+    <!-- -setup two tables[auth and main table tracking]
+  -set up nuxt
+  -connect nuxt and supabase
+  -create a sample in supabase
+  -authentication 
+  -post supabase
+  -upload on supabase
+
+
+
+
+  - frontend 
+  - use cal heatmap  -->
+
+
+  </div>
+</template>
